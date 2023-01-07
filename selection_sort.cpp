30212b3e98a2800e7ff2d69e4968e09d0ac28673
BhavyaGumber <79220494+BhavyaGumber@users.noreply.github.com>
2023-01-07 17:38:29 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,35 +1,27 @@
+#include <stdio.h>
+// #include<bits.stdc
 #include <iostream>
-#include <bits/stdc++.h>
-
 using namespace std;
-void selectionSort(int arr[],int n){
-    int i,j,min_index;
-    for(i=0;i<n-1;i++){
-        min_index=i;
-        for(j=i+1;j<n;j++)
-            if(arr[j]<arr[min_index])
-            min_index=j;
-            if(min_index!=i){
-                swap(arr[min_index], arr[i]);
-            }
-        
-    }
-}
-void printArray(int arr[], int size){
-    int i;
-    for(i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-        cout<<endl;
-    }
-}
 
 int main()
 {
-    int arr[] = {64, 25, 12, 22, 11}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    selectionSort(arr, n); 
-    cout << "Sorted array: \n"; 
-    printArray(arr, n); 
-    return 0; 
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+      cin>>arr[i];
+  }
+  for(int i=0;i<n-1;i++){
+      for(int j=i+1;j<n;j++){
+          if(arr[j]<arr[i]){
+              swap(arr[i],arr[j]);
+          }
+      }
+  }
+  for(int i=0;i<n;i++){
+      cout<<arr[i];
+  }
 
+    return 0;
 }
+
